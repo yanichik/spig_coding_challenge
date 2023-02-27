00ecd7dc16bed469dca96e61a862509e5ec9adf6
yanichik <brunshte@gmail.com>
2023-02-27 13:07:01 -0800

    separate filters: by year, title, author, and isbn

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,50 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
 import Card from "./UI/Card";
 
 function App() {
 	const [books, setBooks] = useState([]);
 	const [filteredBooks, setFilteredBooks] = useState([]);
-	const [yearRange, setYearRange] = useState([]);
-	const filterBksByYearRange = () => {
-		// SOLVE: books complete list only at initial fetch. if multiple filters applied, will not reflect accurate filter
-		setFilteredBooks(() =>
-			books.filter(
-				(book) => book.year >= yearRange[0] && book.year <= yearRange[1]
-			)
-		);
-	};
+	const [isFiltered, setIsFiltered] = useState(false);
 	const handleYrRange = (e) => {
 		e.preventDefault();
 		const yearStart = parseInt(e.target[0].value);
 		const yearEnd = parseInt(e.target[1].value);
 		console.log(yearStart, yearEnd);
-		setYearRange(() => [yearStart, yearEnd]);
-		filterBksByYearRange();
+		const filtered = books.filter(
+			(book) => book.year >= yearStart && book.year <= yearEnd
+		);
+		setFilteredBooks(() => filtered);
+		// SOLVE: books complete list only at initial fetch. if multiple filters applied, will not reflect accurate filter
+	};
+	const handleTitleSearch = (e) => {
+		e.preventDefault();
+		const titleSearched = e.target[0].value;
+		const filtered = books.filter(
+			(book) =>
+				book.title &&
+				book.title.toLowerCase().includes(titleSearched.toLowerCase())
+		);
+		setFilteredBooks(() => filtered);
+	};
+	const handleAuthorSearch = (e) => {
+		e.preventDefault();
+		const authorSearched = e.target[0].value;
+		const filtered = books.filter(
+			(book) =>
+				book.author &&
+				book.author.toLowerCase().includes(authorSearched.toLowerCase())
+		);
+		setFilteredBooks(() => filtered);
+	};
+	const handleIsbnSearch = (e) => {
+		e.preventDefault();
+		const isbnSearched = e.target[0].value;
+		console.log(typeof books[0].isbn);
+		const filtered = books.filter(
+			(book) => book.isbn && book.isbn.toString().includes(isbnSearched)
+		);
+		setFilteredBooks(() => filtered);
 	};
 	useEffect(() => {
 		const url = "/books";
@@ -30,8 +54,8 @@ function App() {
 				const response = await fetch(url);
 				const json = await response.json();
 				console.log(json);
-				setBooks(() =>
-					[...json.body].map((book) => {
+				setBooks((prev) =>
+					[prev, ...json.body].map((book) => {
 						return {
 							title: book.title,
 							author: book.author,
@@ -60,7 +84,28 @@ function App() {
 					Start End:
 					<input type="text" name="yearEnd" placeholder="" />
 				</label>
-				<button type="submit">Filter by Year</button>
+				<button type="submit">Filter by Year Range</button>
+			</form>
+			<form onSubmit={handleTitleSearch}>
+				<label>
+					Title
+					<input type="text" name="title" placeholder="" />
+				</label>
+				<button type="submit">Filter by Title</button>
+			</form>
+			<form onSubmit={handleAuthorSearch}>
+				<label>
+					Author
+					<input type="text" name="author" placeholder="" />
+				</label>
+				<button type="submit">Filter by Author</button>
+			</form>
+			<form onSubmit={handleIsbnSearch}>
+				<label>
+					ISBN
+					<input type="text" name="isbn" placeholder="" />
+				</label>
+				<button type="submit">Filter by ISBN</button>
 			</form>
 			<ul>
 				{filteredBooks.length > 0 ? (
